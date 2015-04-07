8fa9e96d676af546c270fe35c09ac040454cf626
Mathias <mathiasiden@me.com>
2015-04-08 01:20:02 +0200

    New Routes

--- routes/routes.ts
+++ routes/routes.ts
@@ -4,6 +4,7 @@
 /// <reference path="./../typings/tsd.d.ts" />
 import express = require("express");
 import loginHandler = require("./handlers/handlerLogin")
+import newUserHandler = require("./handlers/handlerLogin");
 
 var router = express.Router();
 
@@ -11,7 +12,9 @@ router.get('/', (req, res) => {
     res.send({"message": "Hello"});
 
 });
+
 router.post('/login', loginHandler);
+router.post('/user/new', newUserHandler);
 
 export = router;
 
